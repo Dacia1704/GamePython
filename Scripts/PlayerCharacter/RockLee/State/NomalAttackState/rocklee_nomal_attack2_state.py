e9999ce4a78b601badb693ca29b819e851505a19
QuangHuy-IT <huychanhkun1201@gmail.com>
2024-11-26 21:34:19 +0700

    ok

--- Scripts/PlayerCharacter/RockLee/State/NomalAttackState/rocklee_nomal_attack2_state.py
+++ Scripts/PlayerCharacter/RockLee/State/NomalAttackState/rocklee_nomal_attack2_state.py
@@ -1,12 +1,19 @@
 from Scripts.PlayerCharacter.Base.State.NomalAttackState.nomal_attack_state import NomalAttackState
 import pygame
 from Scripts.game_constants import GameConstants
+from Scripts.Audio.audio_manager import AudioManager
 class RockLeeNomalAttack2State(NomalAttackState):
 
   def enter(self):
     super().enter()
-    self.attack_collider_animations = GameConstants.ROCKLEE_ATTACK2_COLLIDER_ANIMATIONS
     self.update_knock_back_force_target([15,5],150)
+    self.attack_collider_animations = GameConstants.ROCKLEE_ATTACK2_COLLIDER_ANIMATIONS
+    AudioManager.get_instance().play_sfx(self.state_machine.character.nomal_attack_2_sfx_name)
+  
+  def exit(self):
+    super().exit()
+    AudioManager.get_instance().stop_sfx(self.state_machine.character.nomal_attack_2_sfx_name)
+  
   def update(self):
     super().update()
     self.update_sprite_animation(self.state_machine.character.nomal_attack2_spritesheet[0],self.state_machine.character.nomal_attack2_spritesheet[2],False)
--- Scripts/PlayerCharacter/RockLee/State/rocklee_state_machine.py
+++ Scripts/PlayerCharacter/RockLee/State/rocklee_state_machine.py
@@ -4,7 +4,7 @@ from Scripts.PlayerCharacter.RockLee.State.NomalAttackState.rocklee_nomal_attack
 from Scripts.PlayerCharacter.RockLee.State.NomalAttackState.rocklee_nomal_attack3_state import RockLeeNomalAttack3State
 from Scripts.PlayerCharacter.RockLee.State.RockLeeSkillState.rocklee_skill1_state import RockLeeSkill1State
 from Scripts.PlayerCharacter.RockLee.State.RockLeeSkillState.rocklee_skill2_state import RockLeeSkill2State
-from Scripts.PlayerCharacter.Naruto.State.NarutoSkillState.naruto_skill2_state import NarutoSkill2State
+from Scripts.PlayerCharacter.RockLee.State.RockLeeSkillState.rocklee_skill3_state import RockLeeSkill3State
 class RockLeeStateMachine(StateMachine):
   def __init__(self,character,screen_surface):
     super().__init__(character,screen_surface)
@@ -13,3 +13,4 @@ class RockLeeStateMachine(StateMachine):
     self.nomal_attack3 = RockLeeNomalAttack3State(self)
     self.skill1_state = RockLeeSkill1State(self)
     self.skill2_state = RockLeeSkill2State(self)
+    self.skill3_state = RockLeeSkill3State(self)
